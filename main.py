5d6dc15e277234abb214b9d647de8e119c9f547f
jensanf <jensanf@gmail.com>
2019-12-27 13:04:56 +0300

    Commented all print functions

--- main.py
+++ main.py
@@ -6,27 +6,24 @@ import config
 
 os.system("killall mocp")
 os.system("mocp -S")
-
-if __name__ == "__main__":
-	for param in sys.argv:
-		print ("The next track number {}!".format(param))
-		n_song = param
+n_song = sys.argv[1]
+#print ("The next track number {}!".format(n_song))
 client = Client()
 client = Client.from_credentials(config.username, config.password)
 if (n_song == None or n_song == 0):
 	n_song = 1
-print ("START downloading...")
+#print ("START downloading...")
 client.users_likes_tracks()[int(n_song)].track.download('playing.mp3')
-print (" FINISHED!")
+#print (" FINISHED!")
 
 while(1):
-	print ("Track No \"{}\"".format(client.users_likes_tracks()[int(n_song)].track.title))
+	#print ("Track No \"{}\"".format(client.users_likes_tracks()[int(n_song)].track.title))
 	t_dur = client.users_likes_tracks()[int(n_song)].track.duration_ms
 	os.system('mocp -l playing.mp3')
 	n_song = int(n_song) + 1
 	client.users_likes_tracks()[int(n_song)].track.download('buffer.mp3')
-	print ("PLAYING...")
+	#print ("PLAYING...")
 	sleep(t_dur / 1000)
-	print (" OVER.")
+	#print (" OVER.")
 	os.system('mocp -G')
 	os.system('mv buffer.mp3 playing.mp3')
